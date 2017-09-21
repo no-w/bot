3dbd56d6c5b57056ab485376288a8ef451b03574
LauraNOW <ljacobs.now@gmail.com>
2017-09-21 22:53:30 +0200

    21/9

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -17,19 +17,24 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, [ 
+var bot = new builder.UniversalBot(connector, [
     function (session) {
-    // session.send("Salut, bolosse. Prêt à lutter contre le gaspillage alimentaire?");
-    session.beginDialog('greetings3');
+        // session.send("Salut, bolosse. Prêt à lutter contre le gaspillage alimentaire?");
+        session.beginDialog('greetings3');
 
-    // session.send("Ce bolosse vient des dire: %s", session.message.text);
+        // session.send("Ce bolosse vient des dire: %s", session.message.text);
     },
-    function(session, results) {
+    function (session, results) {
         session.beginDialog('choice1');
     },
-    function(session, results) {
+    function (session, results) {
 
-        session.beginDialog('start');
+        session.beginDialog('start1');
+    },
+    function (session, results) {
+       if (results.response.entity == 'J\'ai faim!') {
+           session.beginDialog('faim');
+        }
     }
 ]);
 // Ask the user for their name and greet them by name.
@@ -46,11 +51,11 @@ bot.dialog('greetings2', [
     function (session) {
         var msg = new builder.Message(session);
         msg.attachments([
-        new builder.HeroCard(session)
-            .title("NOW")
-            .subtitle("No Waste, Act Now")
-            .text("Toi aussi, contribue à la lutte contre le gaspillage alimentaire")
-            .images([builder.CardImage.create(session, 'https://raw.githubusercontent.com/no-w/no-w.github.io/master/img/header-bg-now.jpg')])
+            new builder.HeroCard(session)
+                .title("NOW")
+                .subtitle("No Waste, Act Now")
+                .text("Toi aussi, contribue à la lutte contre le gaspillage alimentaire")
+                .images([builder.CardImage.create(session, 'https://raw.githubusercontent.com/no-w/no-w.github.io/master/img/header-bg-now.jpg')])
             // .buttons([
             //     builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
             // ])
@@ -63,11 +68,11 @@ bot.dialog('greetings3', [
     function (session) {
         var msg = new builder.Message(session);
         msg.attachments([
-        new builder.AnimationCard(session)
-            .autoloop(true)
-            .autostart(true)
-            // .title("Tous les jours des tonnes d'aliments sont gaspillés à traver le monde.")
-            .media([builder.CardMedia.create(session,"https://media.giphy.com/media/XmxcpZ7S8cJb2/giphy.gif")])
+            new builder.AnimationCard(session)
+                .autoloop(true)
+                .autostart(true)
+                // .title("Tous les jours des tonnes d'aliments sont gaspillés à traver le monde.")
+                .media([builder.CardMedia.create(session, "https://media.giphy.com/media/XmxcpZ7S8cJb2/giphy.gif")])
         ]);
         session.send(msg).endDialog();
         // session.send(msg);
@@ -75,7 +80,7 @@ bot.dialog('greetings3', [
     }
 ]);
 
-bot.dialog("choice1" , [
+bot.dialog("choice1", [
     function (session) {
         builder.Prompts.text(session, "Es-tu prêt à lutter contre le gaspillage alimentaire?")
     },
@@ -84,15 +89,51 @@ bot.dialog("choice1" , [
     }
 ]);
 
-bot.dialog('start', [
-    function (session) {
-        builder.Prompts.choice(
-            session, 
-            "Je suis le robot NOW, à ton service. Que puis-je faire pour toi?",
-            ["J'ai faim!", "Où est ma bouffe?", "Je veux parler à ton supérieur"], 
-            { listStyle: 3});
+bot.dialog('faim', [
+    function (session, results) {
+        session.beginDialog('localisation');
     },
     function (session, results) {
-        session.endDialog('Tu as choisi %s', results.response.entity)
+        // session.send("Je t'envoie les magains proche de %s", results.response);
+        session.beginDialog('magasin');
     }
 ]);
+
+bot.dialog('start1', [
+    function (session) {
+        //builder.Prompts.text(session, "How many people are in your party?");
+        builder.Prompts.choice(
+            session,
+            "Je suis le robot NOW, à ton service. Que puis-je faire pour toi?",
+            ["J'ai faim!", "Où est ma bouffe?", "Je veux parler à ton supérieur"],
+            { listStyle: 3 });
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+bot.dialog('localisation', [
+    function (session) {
+        builder.Prompts.text(session, "Ou es-tu?");
+    },
+    function (session, results) {
+    //     session.send("Tu es à %s", results.response);
+        session.endDialogWithResult(results);
+    }
+
+]);
+
+bot.dialog('magasin', [
+    function (session, results) {
+        session.endDialogWithResult('Tu es à %s', results.response)
+    }
+
+]);
+
+bot.dialog('help', function (session) {
+    session.endDialog("Comment pouvons-nous vous aider?");
+})
+.triggerAction({
+    matches: /^help$/i,
+});
